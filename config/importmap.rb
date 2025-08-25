c2868894fc802982c2b0aeff6fd34dd6c049f195
Leonardo Balestrin Depelegrin <leobd2506@gmail.com>
2025-08-25 16:36:57 -0300

    feat: Add style

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.7.2/js/all.js" # @7.0.0
